9f24c0a38f0b665b2f17507a47c44f800e39a767
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2022-03-22 19:48:40 -0300

    fix: add integration tests

--- src/test/ar/edu/ungs/hangman/core/sessions/domain/SessionMother.java
+++ src/test/ar/edu/ungs/hangman/core/sessions/domain/SessionMother.java
@@ -2,8 +2,14 @@ package ar.edu.ungs.hangman.core.sessions.domain;
 
 import ar.edu.ungs.hangman.core.words.domain.WordMother;
 
+import java.util.List;
+
 public final class SessionMother {
     public static Session random(){
         return new Session("user", WordMother.random());
+    }
+
+    public static List<Session> randoms(){
+        return List.of(random(), random(), random(), random());
     }
 }
--- src/test/ar/edu/ungs/hangman/core/sessions/infrastructure/InMemorySessionRepositoryTest.java
+++ src/test/ar/edu/ungs/hangman/core/sessions/infrastructure/InMemorySessionRepositoryTest.java
@@ -1,7 +1,40 @@
 package ar.edu.ungs.hangman.core.sessions.infrastructure;
 
-import static org.junit.jupiter.api.Assertions.*;
+import ar.edu.ungs.hangman.core.sessions.domain.Session;
+import ar.edu.ungs.hangman.core.sessions.domain.SessionMother;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-class InMemorySessionRepositoryTest {
+import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+final class InMemorySessionRepositoryTest {
+    private InMemorySessionRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        this.repository = new InMemorySessionRepository();
+    }
+
+    @Test
+    void should_find_session_by_user() {
+        List<Session> sessions = SessionMother.randoms();
+
+        sessions.forEach(session -> repository.save(session));
+
+        Session expected = sessions.stream().findAny().get();
+
+        Optional<Session> actual = repository.findByUser(expected.user());
+
+        assertTrue(actual.isPresent());
+    }
+
+    @Test
+    void should_save_session() {
+        List<Session> sessions = SessionMother.randoms();
+
+        sessions.forEach(session -> repository.save(session));
+    }
 }
