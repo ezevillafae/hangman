4e3576c6ceb64fa9a366b8e36a404e8a0a3b55da
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2022-03-24 15:15:56 -0300

    feat: Add SessionGuesser.java

--- src/main/ar/edu/ungs/hangman/core/sessions/application/guess/SessionGuesser.java
+++ src/main/ar/edu/ungs/hangman/core/sessions/application/guess/SessionGuesser.java
@@ -1,4 +1,37 @@
 package ar.edu.ungs.hangman.core.sessions.application.guess;
 
+import ar.edu.ungs.hangman.core.sessions.application.SessionResponse;
+import ar.edu.ungs.hangman.core.sessions.application.tries.SessionTryer;
+import ar.edu.ungs.hangman.core.sessions.domain.DomainSessionFinder;
+import ar.edu.ungs.hangman.core.sessions.domain.Session;
+
+import java.util.List;
+
 public final class SessionGuesser {
+	private final static String USER_MACHINE = "machine";
+	private final static List<Character> VOWELS = List.of('A', 'E', 'I', 'O', 'U');
+	private final static List<Character> CONSONATS = List.of('B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Z', 'W', 'Y');
+
+	private final DomainSessionFinder sessionFinder;
+	private final SessionTryer sessionTryer;
+
+	public SessionGuesser(DomainSessionFinder sessionFinder, SessionTryer sessionTryer) {
+		this.sessionFinder = sessionFinder;
+		this.sessionTryer = sessionTryer;
+	}
+
+	public SessionResponse guess() {
+		Session session = this.sessionFinder.find(USER_MACHINE);
+
+		for (Character character: VOWELS){
+			sessionTryer.execute(USER_MACHINE, character);
+		}
+
+		for (Character consonant: CONSONATS){
+			sessionTryer.execute(USER_MACHINE, consonant);
+		}
+
+		return SessionResponse.map(session);
+	}
 }
+
--- src/test/ar/edu/ungs/hangman/core/sessions/application/guess/SessionGuesserTest.java
+++ src/test/ar/edu/ungs/hangman/core/sessions/application/guess/SessionGuesserTest.java
@@ -1,7 +1,37 @@
 package ar.edu.ungs.hangman.core.sessions.application.guess;
 
-import static org.junit.jupiter.api.Assertions.*;
+import ar.edu.ungs.hangman.core.sessions.application.tries.SessionTryer;
+import ar.edu.ungs.hangman.core.sessions.domain.DomainSessionFinder;
+import ar.edu.ungs.hangman.core.sessions.domain.SessionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-class SessionGuesserTest {
+import static org.mockito.Mockito.mock;
 
+final class SessionGuesserTest {
+	private SessionGuesser guesser;
+
+	private SessionRepository repository;
+	private DomainSessionFinder sessionFinder;
+	private SessionTryer sessionTryer;
+
+	@BeforeEach
+	void setUp() {
+
+		this.sessionFinder = mock(DomainSessionFinder.class);
+		this.repository = mock(SessionRepository.class);
+		this.sessionTryer = new SessionTryer(this.sessionFinder, repository);
+
+		this.guesser = new SessionGuesser(sessionFinder, sessionTryer);
+	}
+
+	@Test
+	void should_guess_the_word() {
+
+	}
+
+	@Test
+	void should_fail_the_guess() {
+
+	}
 }
