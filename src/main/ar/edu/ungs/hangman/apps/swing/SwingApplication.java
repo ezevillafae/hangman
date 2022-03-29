d73e27c021eeacff1f1c0275f9cb65b1085d96bc
Francisco Ruiz <francisco.ruiz@mercadolibre.com>
2022-03-29 17:04:19 -0300

    feature: add swing application skeleton

--- src/main/ar/edu/ungs/hangman/apps/swing/SwingApplication.java
+++ src/main/ar/edu/ungs/hangman/apps/swing/SwingApplication.java
@@ -5,14 +5,44 @@ import ar.edu.ungs.hangman.apps.Application;
 import javax.swing.*;
 
 public final class SwingApplication extends Application {
+	private static final String TITLE = "HANGMAN";
+	private static final String BUTTON_TEXT = "TRY";
+
+	private static final Integer MARGIN = 20;
+	private static final Integer WIDTH = 400;
+	private static final Integer HEIGHT = 500;
+
+	private final JFrame frame;
+	private final JLabel title;
+	private final JButton button;
+	private final JTextField textField;
+
+	public SwingApplication() {
+		this.frame = new JFrame();
+		this.title = new JLabel(TITLE);
+		this.button = new JButton(BUTTON_TEXT);
+		this.textField = new JTextField();
+
+		setup();
+	}
+
+	private void setup() {
+		title.setBounds(160, 20, 250, 10);
+		button.setBounds(280, 400, 100, 50);
+		textField.setBounds(MARGIN, 400, 250, 50);
+
+		frame.add(title);
+		frame.add(button);
+		frame.add(textField);
+
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+
 	@Override
 	public void run() {
-		JFrame frame = new JFrame("SwingApplication");
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
-		frame.setSize(500, 500);
+		SwingApplication application = new SwingApplication();
 	}
 
 }
