adb48a1b4b28173e32b2b4b68eaa308c575a7892
Francisco Ruiz <francisco.ruiz@mercadolibre.com>
2022-03-31 11:07:38 -0300

    fix: Delete constructor of the BaseView class

--- src/main/ar/edu/ungs/hangman/apps/swing/SwingApplication.java
+++ src/main/ar/edu/ungs/hangman/apps/swing/SwingApplication.java
@@ -1,9 +1,11 @@
 package ar.edu.ungs.hangman.apps.swing;
 
 import ar.edu.ungs.hangman.apps.Application;
+import ar.edu.ungs.hangman.apps.swing.views.MainView;
 
 public final class SwingApplication implements Application {
 	@Override
 	public void run() {
+		new MainView();
 	}
 }
--- src/main/ar/edu/ungs/hangman/apps/swing/views/BaseView.java
+++ src/main/ar/edu/ungs/hangman/apps/swing/views/BaseView.java
@@ -3,9 +3,5 @@ package ar.edu.ungs.hangman.apps.swing.views;
 import javax.swing.*;
 
 public abstract class BaseView extends JFrame {
-	public BaseView() {
-		setupComponents();
-	}
-
 	abstract void setupComponents();
 }
